cc269f6a624c1150b0197cd8d665223c31e4c3e7
tirafesi <tirafesi@hotmail.com>
2018-04-14 17:12:25 +0100

    close #8

--- Client/src/cli/MainMenu.cs
+++ Client/src/cli/MainMenu.cs
@@ -23,30 +23,35 @@ namespace Client.Cli
         public const int MONEY = 1;
 
         /// <summary>
+        /// Index of edit quote menu entry.
+        /// </summary>
+        public const int EDIT_QUOTE = 2;
+
+        /// <summary>
         /// Index of emit selling order menu entry.
         /// </summary>
-        public const int SELLING_ORDER = 2;
+        public const int SELLING_ORDER = 3;
 
         /// <summary>
         /// Index of emit purchase order menu entry.
         /// </summary>
-        public const int PURCHASE_ORDER = 3;
+        public const int PURCHASE_ORDER = 4;
 
         /// <summary>
         /// Index of list pending orders menu entry.
         /// </summary>
-        public const int PENDING_ORDERS = 4;
+        public const int PENDING_ORDERS = 5;
 
         /// <summary>
         /// Index of exit menu entry.
         /// </summary>
-        public const int EXIT = 5;
+        public const int EXIT = 6;
 
 
         /* --- METHODS --- */
 
         public MainMenu(Client client) : base(client, "Main Menu",
-            new string[] {"Logout", "Add Money", "Emit Selling Order", "Emit Purchase Order", "List Pending Orders", "Exit" })
+            new string[] {"Logout", "Add Money", "Edit current quote", "Emit Selling Order", "Emit Purchase Order", "List Pending Orders", "Exit" })
         {
 
         }
@@ -70,6 +75,63 @@ namespace Client.Cli
                             Console.Write("Amount: ");
                             long amount = Convert.ToInt64(Console.ReadLine());
                             client.AddMoney(amount);
+
+                            break;
+                        }
+
+                    case EDIT_QUOTE:
+                        {
+                            List<Order> orders = client.GetPendingOrders();
+                            if (orders.Count == 0)
+                            {
+                                break;
+                            }
+                            bool selling = false;
+                            bool purchase = false;
+                            foreach(Order order in orders)
+                            {
+                                if (!selling && order.Type == OrderType.Selling)
+                                {
+                                    selling = true;
+                                }
+                                else if (!purchase && order.Type == OrderType.Purchase)
+                                {
+                                    purchase = true;
+                                }
+
+                                if (purchase && selling)
+                                {
+                                    break;
+                                }
+                            }
+
+                            double currentQuote = client.GetQuote();
+                            Console.WriteLine("Current Quote: " + currentQuote);
+
+                            double quote = 0;
+                            if (selling && purchase)
+                            {
+                                Console.Write("New Quote: ");
+                                quote = Convert.ToDouble(Console.ReadLine());
+                            }
+                            else if (selling)
+                            {
+                                do
+                                {
+                                    Console.Write("New Quote (less or equal than current quote): ");
+                                    quote = Convert.ToDouble(Console.ReadLine());
+                                } while (quote > currentQuote);
+                            }
+                            else if (purchase)
+                            {
+                                do
+                                {
+                                    Console.Write("New Quote (higher or equal than current quote): ");
+                                    quote = Convert.ToDouble(Console.ReadLine());
+                                } while (quote < currentQuote);
+                            }
+
+                            client.AddQuote(quote);
 
                             break;
                         }
